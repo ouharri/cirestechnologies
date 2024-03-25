8573dc866dbfcdcf79ee24d8fa186e539a7d2b03
ouharri <ouharrioutman@gmail.com>
2024-03-25 12:51:25 +0000

    Configuration class for OpenAPI documentation settings.

--- src/main/java/com/ouharri/cirestechnologies/core/OpenApiConfig.java
+++ src/main/java/com/ouharri/cirestechnologies/core/OpenApiConfig.java
@@ -1,11 +1,10 @@
-package com.ouharri.aftas.core;
+package com.ouharri.cirestechnologies.core;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -22,27 +21,15 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         email = "ouharri.outman+spring@gmail.com",
                         url = "https://ouharri.ma"
                 ),
-                description = "OpenApi documentation for Jobinow API",
-                title = "OpenApi specification - ouharri",
+                description = "OpenApi documentation for cirestechnologies Challenge API",
+                title = "cirestechnologies - ouharri",
                 version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://some-url.com"
-                ),
                 termsOfService = "Terms of service"
         ),
         servers = {
                 @Server(
-                        description = "PROD ENV",
-                        url = "http://54.162.210.15:8085"
-                ),
-                @Server(
-                        description = "Test ENV",
-                        url = "http://localhost:8084    "
-                ),
-                @Server(
                         description = "Local ENV",
-                        url = "http://localhost:8085"
+                        url = "http://localhost:9090"
                 )
         },
         security = {
